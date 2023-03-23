4b3557d66171f51b0346a17d3e913333a07a43b4
Ibrahim Mowafy <ibrahim.mowafy10@gmail.com>
2023-03-23 12:08:05 +0200

    Implement home screen

--- app/index.js
+++ app/index.js
@@ -1,10 +1,54 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES } from '../constants';
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from '../components';
+import useFetch from '../hook/useFetch';
 
 const Home = () => {
+  const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState('');
+  const { data, isLoading, error } = useFetch('search', {
+    query: 'React Developer',
+    num_page: 1,
+  });
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: '',
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ padding: SIZES.medium }}>
+          <Welcome
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              if (searchTerm) {
+                router.push(`/search/${searchTerm}`);
+              }
+            }}
+          />
+          <Popularjobs data={data} error={error} isLoading={isLoading} />
+          <Nearbyjobs data={data} error={error} isLoading={isLoading} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
